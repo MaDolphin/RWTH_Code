3af91ce0a908992a1ae136e0965c010e238934dc
Haikun Huang <MaDolphin@users.noreply.github.com>
2019-11-07 17:55:54 +0100

    uodate

--- Others/Python/test.py
+++ Others/Python/test.py
@@ -1,9 +1,11 @@
 import numpy as np
-n = 6
-res = np.zeros((n, n))
+# n = 6
+# res = np.zeros((n, n))
 
-# r = (x+y)*(x+y+1)/2 + x
-for x in range(n):
-    for y in range(n):
-        res[x][y] = (x+y)*(x+y+1)/2 + y
-print(res)
+# # r = (x+y)*(x+y+1)/2 + x
+# for x in range(n):
+#     for y in range(n):
+#         res[x][y] = (x+y)*(x+y+1)/2 + y
+# print(res)
+
+print(np.random.rand())
