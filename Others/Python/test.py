84eff7d267a67f66baa8565086557cc7f92506a5
Haikun Huang <MaDolphin@users.noreply.github.com>
2019-11-26 16:10:54 +0100

    update

--- Others/Python/test.py
+++ Others/Python/test.py
@@ -1,11 +1,27 @@
 import numpy as np
-# n = 6
-# res = np.zeros((n, n))
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
-# # r = (x+y)*(x+y+1)/2 + x
-# for x in range(n):
-#     for y in range(n):
-#         res[x][y] = (x+y)*(x+y+1)/2 + y
-# print(res)
+#构造变量
+number=20 
+x=np.linspace(0,10,number) #x值
+X=sm.add_constant(x) #回归方程添加一列x0=1
+bata=np.array([2,5]) #系数
+e=np.random.normal(size=number) #误差
+y=np.dot(X,bata)+e #观察值y
 
-print(np.random.rand())
+#建回归方程
+#OLS（endog,exog=None,missing='none',hasconst=None) (endog:因变量，exog=自变量）
+modle=sm.OLS(y,X) #最小二乘法
+res=modle.fit() #拟合数据
+Bata=res.params  #取系数
+print(Bata)
+print(res.summary()) #结果
+
+#作图
+Y=res.fittedvalues  #预测值
+fig, ax = plt.subplots(figsize=(8,6))
+ax.plot(x, y, 'o', label='data')#原始数据
+ax.plot(x, Y, 'r--.',label='test')#拟合数据
+ax.legend(loc='best') #展示各点表示意思，即label
+plt.show()
