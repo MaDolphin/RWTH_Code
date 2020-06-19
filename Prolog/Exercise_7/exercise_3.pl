867717447dd6f43ae2f195fff2c0d88d83115e43
Haikun Huang <MaDolphin@users.noreply.github.com>
2020-06-19 02:21:48 +0200

    Update exercise_3.pl

--- Prolog/Exercise_7/exercise_3.pl
+++ Prolog/Exercise_7/exercise_3.pl
@@ -22,3 +22,14 @@ list_length([_|Xs], L):- list_length(Xs,N), L is N+1.
 
 % c)
 
+do_list(N, L) :- do_list1(N, [], L).
+do_list1(0, L, L) :- !.
+do_list1(N, R, L) :- N > 0, N1 is N-1, do_list1(N1, [N|R], L).
+
+delete(Element,[Element|Tail],Tail).
+delete(Element,[Head|Tail],[Head|Tail1]) :- delete(Element,Tail,Tail1).
+
+find_powerSym(_,[],List,R):- member(R,List).
+find_powerSym(X,[H|T],List,R):- X^^H =:= H^^X, find_powerSym(X,T,List,R).
+find_powerSym(X,[H|T],List,R):- X^^H =\= H^^X, delete(H,List,List1), find_powerSym(X,T,List1,R).
+powerSym(X,R):- do_list(X,List),find_powerSym(X,List,List,R).
